2421fc8228e8c6854d995d5157fc7706051762f7
unknown <shilpas2101@gmail.com>
2024-06-10 23:40:34 +0530

    frontend uploaded

---
+++ src/components/AddEmployee.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+
+const AddEmployee = () => {
+    const [datas,changeData]=useState(
+        {
+            "name":"", 
+            "empid":"",
+            "dob":"",
+            "email":""
+            
+        }
+    )
+    const inputHandler=(event)=>{
+        changeData({...datas,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(datas)
+    }
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input type="text" className="form-control" name='name' value={datas.name} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">ID</label>
+                            <input type="text" className="form-control" name='empid' value={datas.empid} onChange={inputHandler} />
+                        </div>
+                        
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">D-O-B</label>
+                            <input type="date" name="dob" value={datas.dob} id="" onChange={inputHandler} className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email</label>
+                            <input type="email" name="email" value={datas.email} onChange={inputHandler} id="" className="form-control" />
+                        </div>
+                       
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-primary" onClick={readValue}>Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddEmployee
